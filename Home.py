1cb6161a2027fc6e5685c138cd20d4ce30b3c2ed
saranvishu <141228739+saranvishu@users.noreply.github.com>
2025-05-03 15:58:19 +0530

    Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
-
+# pages/Home.py
 import streamlit as st
+
 def show():
-    st.title("🏠 Home")
-    st.write("Welcome to the YouTube Sentiment Tracker App!")
+    st.title("Home")
+    st.write("Welcome to the Home page!")
+
